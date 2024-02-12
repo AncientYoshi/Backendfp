11ebc57573e1467327179b1aa0e597cba1d5b588
AncientYoshi <paiswanpyae2002@gmail.com>
2024-02-12 10:52:51 +0630

    Updated

--- Restaurant/serializers.py
+++ Restaurant/serializers.py
@@ -1,10 +1,14 @@
 # serializers.py
 
 from rest_framework import serializers
-from .models import Menu  # Import your Menu model here
+from .models import Menu, Booking  # Import your Menu model here
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ['id', 'Title', 'Inventory', 'Price']
 
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,8 +1,8 @@
 # views.py
 
-from rest_framework import generics
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework import generics, viewsets
+from .models import Menu,Booking
+from .serializers import MenuSerializer , BookingSerializer
 
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
@@ -11,3 +11,7 @@ class MenuItemView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
